e7680173a6e9877c3696382ce70c27c803768273
Alessandro Serpi <alessandro.serpi@me.com>
2019-12-22 00:07:46 +0100

    Add main function

--- rclone_sync.py
+++ rclone_sync.py
@@ -2,3 +2,14 @@
 
 For more info: https://github.com/aserpi/rclone-sync
 """
+
+__version__ = 0.1
+
+
+def main() -> None:
+    """Main function."""
+    raise NotImplementedError
+
+
+if __name__ == "__main__":
+    main()
