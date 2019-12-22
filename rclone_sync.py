f033574aa809480824832bfe6e60b4f8ea5cf7a1
Alessandro Serpi <alessandro.serpi@me.com>
2019-12-22 16:11:07 +0100

    Add option to print script version

--- rclone_sync.py
+++ rclone_sync.py
@@ -2,12 +2,21 @@
 
 For more info: https://github.com/aserpi/rclone-sync
 """
+import argparse
+import sys
 
-__version__ = 0.1
+__version__ = "0.0.1"
 
 
 def main() -> None:
     """Main function."""
+    parser = argparse.ArgumentParser(description="Bi-directional sync for rclone.")
+
+    # Optional arguments
+    parser.add_argument("-V", "--version", action="version", version=f"%(prog)s v{__version__}")
+
+    args = parser.parse_args()
+
     raise NotImplementedError
 
 
